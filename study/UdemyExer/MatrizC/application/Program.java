bcadc394c03f4c54356f360a7dd41aedd551ce8f
Jorge Baruja Mareco <jbaruja@gmail.com>
2022-02-11 07:27:50 -0300

    Udem Matriz

--- study/UdemyExer/MatrizC/application/Program.java
+++ study/UdemyExer/MatrizC/application/Program.java
@@ -1,6 +1,6 @@
-package br.com.jorge.study.UdemyExer.MatrizB.application;
 
-import java.util.Arrays;
+package br.com.jorge.study.UdemyExer.MatrizC.application;
+
 import java.util.Scanner;
 
 /**
@@ -8,24 +8,23 @@ import java.util.Scanner;
  * @author home
  */
 public class Program {
-
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner teclado = new Scanner(System.in);
-
+        
         int M = teclado.nextInt();
         int N = teclado.nextInt();
-
+        
         Integer [][] mat = new Integer [M][N];
-
+        
         for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
+            for (int j = 0; j< mat[i].length; j++){
                 mat[i][j] = teclado.nextInt();
             }
         }
         
         int ver = teclado.nextInt();
         
-        for(int i=0; i<mat.length; i++){
+        for(int i =0; i<mat.length; i++){
             for(int j = 0; j<mat[i].length; j++){
                 if(mat[i][j] == ver){
                     System.out.println("Posição "+i+","+j+":");
@@ -36,7 +35,7 @@ public class Program {
                         System.out.println("Up: " + mat[i-1][j]);
                     }
                     if(j < mat[i].length-1){
-                        System.out.println("Right: " +mat[i][j+1]);
+                        System.out.println("Right: " + mat[i][j+1]);
                     }
                     if(i < mat.length-1){
                         System.out.println("Down: " + mat[i+1][j]);
@@ -45,7 +44,5 @@ public class Program {
             }
         }
         
-        
-
     }
 }
